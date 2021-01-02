6a0fdc6e4932d66a21fc0a7bb5a15f0135d4babe
rishikaushal2009 <rishikaushal2009@gmail.com>
2021-01-03 04:42:57 +0530

    russian 2

---
+++ russian-grid.py
@@ -0,0 +1,132 @@
+ 
+def maze2graph(maze):
+    height = len(maze)
+    width = len(maze[0]) if height else 0
+    graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]}
+    for row, col in graph.keys():
+        if row < height - 1 and not maze[row + 1][col]:
+            graph[(row, col)].append(("S", (row + 1, col)))
+            graph[(row + 1, col)].append(("N", (row, col)))
+        if col < width - 1 and not maze[row][col + 1]:
+            graph[(row, col)].append(("E", (row, col + 1)))
+            graph[(row, col + 1)].append(("W", (row, col)))
+    return graph
+
+
+from collections import deque
+
+
+def find_path_bfs(maze):
+    import ipdb; ipdb.set_trace()
+    a = []
+    parent ={}
+    start, goal = (0, 0), (len(maze) - 1, len(maze[0]) - 1)
+    queue = deque([("", start)])
+    visited = set()
+    graph = maze2graph(maze)
+    while queue:
+        path, current = queue.popleft()
+        # path, current = queue.pop()
+        if current == goal:
+            '''
+            for i,j in queue :
+                a.append(j)    #for k in j :
+            print([t for t in (set(tuple(i) for j,i in queue))] )
+            '''
+            p=[]
+            while current is not None :
+                p.append(current)
+                current = parent[current]
+            print(p.reverse())    
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        
+        for direction, neighbour in graph[current]:
+            parent[neighbour]=current
+            queue.append((path + direction, neighbour))
+    return "NO WAY!"
+
+
+def find_path_dfs(maze):
+    start, goal = (1, 1), (len(maze) - 2, len(maze[0]) - 2)
+    stack = deque([("", start)])
+    visited = set()
+    graph = maze2graph(maze)
+    while stack:
+        path, current = stack.pop()
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            stack.append((path + direction, neighbour))
+    return "NO WAY!"
+
+
+from heapq import heappop, heappush
+
+
+def heuristic(cell, goal):
+    return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
+
+
+def find_path_astar(maze):
+    start, goal = (1, 1), (len(maze) - 2, len(maze[0]) - 2)
+    pr_queue = []
+    heappush(pr_queue, (0 + heuristic(start, goal), 0, "", start))
+    visited = set()
+    graph = maze2graph(maze)
+    while pr_queue:
+        _, cost, path, current = heappop(pr_queue)
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            heappush(pr_queue, (cost + heuristic(neighbour, goal), cost + 1,
+                                path + direction, neighbour))
+    return "NO WAY!"
+
+'''
+MAZE = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+'''
+
+ 
+
+
+MAZE = [
+    
+    [0,0,1,1,0],              
+    [0,1,0,0,0],               
+    [0,1,0,1,0],                  
+    [0,0,0,1,0],
+    [0,0,0,1,0]
+     
+]
+
+def find_shortest_path(grid4, grid4_start, grid4_target):
+    print(maze2graph(MAZE))
+    print(find_path_bfs(MAZE))
+    '''
+    a,c = find_path_bfs(MAZE)
+    print(a)
+    print(c)
+    '''
+    
